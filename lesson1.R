dd94829bd1ced0062ea86b2e3535b2c024281e78
unknown <4567031@mail.ru>
2016-02-05 16:11:47 +0300

    first lesson

---
+++ lesson1.R
@@ -0,0 +1,15 @@
+a=3
+b=2
+a+b
+d=1:100
+f=sin(d)
+plot(d,f)
+t=c(T,TRUE)
+f=c(F,FALSE)
+#а=3,4,1-numeric(2)
+#b="x","y","2"-CHARACTER(1)
+#c=T,F,T,FALSE,TRUE-LOGIC(3)
+f=c(1,0,T)
+plot(density(rnorm(1:100)),col="red")
+#логические операторы
+# двоейное равно(==)
